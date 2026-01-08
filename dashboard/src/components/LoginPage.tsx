104fb5f01eaf5cf8f3ea9b49aa3deb77eda04cfd
gauciv <vincentaugusto16@gmail.com>
2026-01-08 23:45:47 +0800

    test: modify ci script

--- dashboard/src/components/LoginPage.tsx
+++ dashboard/src/components/LoginPage.tsx
@@ -612,7 +612,6 @@ const LoginPage: React.FC = () => {
                             </button>
                         </form>
                     )}
-{/* mock */}
                     {/* Dev Mode Notice */}
                     {!isAzureHosted() && mode === 'login' && (
                         <div className="p-3 rounded-lg bg-yellow-500/10 border border-yellow-500/20">
